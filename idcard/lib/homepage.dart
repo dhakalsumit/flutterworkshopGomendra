ea7ed2bef1c8cd0c5b2ca6c9ee72092566a89c0c
Sumit Dhakal <dhakalsumeet123@gmail.com>
2025-02-18 08:06:26 +0545

    push

--- idcard/lib/homepage.dart
+++ idcard/lib/homepage.dart
@@ -6,16 +6,47 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text(
-        "MERO TUTOR PVT. LTD.",
-        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      appBar: AppBar(
+        title: Text(
+          "MERO TUTOR PVT. LTD.",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        leading: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: CircleAvatar(
+            backgroundImage: AssetImage("../images/meroimage.jpg"),
+          ),
+        ),
       ),
-      centerTitle: true,
-      backgroundColor: Colors.blue,
-      leading: CircleAvatar(
-        backgroundImage: AssetImage("../images/meroimage.jpg"),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "ID CARD",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            CircleAvatar(
+              radius: 100,
+              backgroundImage: AssetImage("../images/sudip.jpg"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "SUDIP ADHIKARI",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
