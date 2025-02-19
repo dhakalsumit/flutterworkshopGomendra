40aa801cd6099fd93c8eaace5a43d5edf5742306
Sumit Dhakal <dhakalsumeet123@gmail.com>
2025-02-19 08:27:52 +0545

    ui

--- volleyballscore/lib/homepage.dart
+++ volleyballscore/lib/homepage.dart
@@ -8,6 +8,19 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  int teamAscore = 0;
+  int teamBscore = 0;
+
+  void incrementTeamA() {
+    setState(() {
+      teamAscore++;
+    });
+  }
+
+  void incrementTeamB() {
+    teamBscore++;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +33,9 @@ class _HomepageState extends State<Homepage> {
         ),
         centerTitle: true,
       ),
+      persistentFooterButtons: [
+        Center(child: Text("Developed by XYZ")),
+      ],
       body: Center(
         child: Column(
           children: [
@@ -28,6 +44,70 @@ class _HomepageState extends State<Homepage> {
             ),
             Text("Volleyball Score Board Application",
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300)),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('A',
+                    style:
+                        TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+                Text('B',
+                    style:
+                        TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(teamAscore.toString(),
+                    style:
+                        TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+                Text(teamBscore.toString(),
+                    style:
+                        TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                    onPressed: () {
+                      incrementTeamA();
+                    },
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    )),
+                ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                    onPressed: () {
+                      incrementTeamB();
+                    },
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    )),
+              ],
+            ),
+            SizedBox(
+              height: 70,
+            ),
+            IconButton(
+                iconSize: 50,
+                color: Colors.black,
+                onPressed: () {},
+                icon: Icon(Icons.restore))
           ],
         ),
       ),
